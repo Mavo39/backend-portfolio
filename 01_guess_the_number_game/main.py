4a8a728b82067d2ea91f71dba0704a49c8070ba2
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-23 20:57:40 +0900

    feat: add mode selection for limited or unlimited attempts

--- 01_guess_the_number_game/main.py
+++ 01_guess_the_number_game/main.py
@@ -9,11 +9,12 @@ def get_integer(prompt):
         try:
             return int(input(prompt))
         except ValueError:
-            print("Please enter a valid integer.")
+            print("Please enter a valid integer.\n")
 
 # 最小値と最大値の入力
 n = get_integer("Input minimum number: ")
 m = get_integer("Input maximum number: ")
+print()
 
 while n >= m:
    input("Your inputed minimum number was bigger than maximum one. Please input again. (Press Enter)")
@@ -21,3 +22,8 @@ while n >= m:
 
 answer = random.randint(n,m)
 
+mode = 0
+while mode not in [1,2]:
+    mode = get_integer("\nPlease select integer 1 or 2\n1: Limited attempts\n2: Unlimited attempts\nYour select: ")
+
+print(f"\nYou can input between {n} to {m}\n")

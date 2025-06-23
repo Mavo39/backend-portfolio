5e97ab931ac8dabf0ebe17b09246497cfd0e7260
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-23 19:17:20 +0900

    feat: Implement get_integer function to check valid integer

---
+++ 01_guess_the_number_game/main.py
@@ -0,0 +1,21 @@
+import sys
+import random
+
+input('Welcome to "Guess the number game"!(Press Enter)')
+
+# 整数を入力するための関数
+def get_integer(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid integer.")
+
+# 最小値と最大値の入力
+n = get_integer("Input minimum number: ")
+m = get_integer("Input maximum number: ")
+
+while n >= m:
+   input("Your inputed minimum number was bigger than maximum one. Please input again. (Press Enter)")
+   m = get_integer("Input maximum number: ")
+

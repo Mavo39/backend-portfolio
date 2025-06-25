21be0d745ab2a43e9bc9789f3fa6e37c1cdba2e2
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-25 21:11:48 +0900

    refactor: add comment when game over in mode 1

--- 01_guess_the_number_game/main.py
+++ 01_guess_the_number_game/main.py
@@ -1,7 +1,7 @@
 import sys
 import random
 
-print("Welcome to 'Guess the number game'!\nFirst, you'll set the minimum and maximum numbers.\nThen, try to guess the number chosen within that range.(Press Enter)\n")
+print("Welcome to 'Guess the number game'!\nFirst, you'll set the minimum and maximum numbers.\nThen, try to guess the number chosen within that range.\n")
 
 # 整数を入力するための関数
 def get_integer(prompt):
@@ -30,12 +30,12 @@ while mode not in [1,2]:
     mode = get_integer("Please select game mode 1 or 2 (1: Limited attempts / 2: Unlimited attempts)\nYour select is: ")
     print()
 
-print(f"You can input between {n} and {m}\n")
+print(f"You can input between {n} and {m}")
 
 # メッセージ
-msg_congrats = "Congratulations!!\nPress Enter to finish the game."
-msg_game_over = "Game Over... The correct answer was {answer}.\nPress Enter to finish the game."
-msg_try_again = "Try again! (Press Enter)"
+msg_congrats = "Congratulations!!"
+msg_game_over = "Game Over... The correct answer was {answer}."
+msg_try_again = " Try again!"
 msg_input_number = "\nYour input number is: "
 msg_out_of_range = "Your input is out of range ({n} - {m})!"
 msg_high = "Too high!"
@@ -48,6 +48,7 @@ if mode == 1:
 
     while attempts < 1 or attempts > max_attempts:
         attempts = get_integer(f"How many attempts do you want to try???(Max: {max_attempts})\nAttemps: ")
+
         if attempts > max_attempts: 
             print(f"You can try up to {max_attempts} attempts.\nPlease input within that range.\n")
         elif attempts < 1:
@@ -62,11 +63,12 @@ if mode == 1:
         elif guess < n or guess > m:
             print(msg_out_of_range.format(n=n, m=m))
         elif guess > answer:
-            print(msg_high)
-            print(msg_try_again)
+            print(msg_high + msg_try_again)
         else:
-            print(msg_low)
-            print(msg_try_again)
+            print(msg_low + msg_try_again)
+    
+    else:
+        print(msg_game_over.format(answer=answer))
 
 # モード2
 else:
@@ -79,8 +81,6 @@ else:
         elif guess < n or guess > m:
             print(msg_out_of_range.format(n=n, m=m))
         elif guess > answer:
-            print(msg_high)
-            print(msg_try_again)
+            print(msg_high + msg_try_again)
         else:
-            print(msg_low)
-            print(msg_try_again)
+            print(msg_low + msg_try_again)

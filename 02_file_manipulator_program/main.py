e7b714baae09a24418e0dc106c4f9a093137650b
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-07-01 21:32:57 +0900

    feat: add copy_file function

--- 02_file_manipulator_program/main.py
+++ 02_file_manipulator_program/main.py
@@ -1,3 +1,29 @@
 import sys
 import os
 
+# reverse
+def reverse_file(inputpath, outputpath):
+    # ファイルの読み込み
+    with open(inputpath, 'r', encoding="utf-8") as f:
+        contents = f.read()
+    
+    # ファイルに書き込み
+    with open(outputpath, 'w', encoding='utf-8') as f:
+        f.write(contents[::-1])
+    
+# copy
+def copy_file(inputpath, outputpath):
+    # ファイルの読み込み
+    with open(inputpath, 'r', encoding='utf-8') as f:
+        contents = f.read()
+    
+    # ファイルに書き込み
+    with open(outputpath, 'w', encoding='utf-8') as f:
+        f.write(contents)
+
+
+# duplicate-contents
+
+
+# replace-string
+

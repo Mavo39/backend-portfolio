ccaccb542d23409be61acf66ba5a2130f670f2de
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-07-30 21:28:24 +0900

    feat: add is_valid_inputfile_path to check if path from second CLI argument exists

--- 03_markdown_to_HTML_converter/file-converter.py
+++ 03_markdown_to_HTML_converter/file-converter.py
@@ -5,3 +5,6 @@ import markdown
 def is_markdown():
     return len(sys.argv) > 1 and sys.argv[1] == "markdown"
 
+def is_valid_inputfile_path():
+    return len(sys.argv) > 2 and os.path.exists(sys.argv[2])
+

d66f18432e743cd27b6e36a3e672c6aa672c318b
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-01 16:43:53 +0900

    feat: add is_inputfile_markdown function to check if inputfile has .md extension

--- 03_markdown_to_HTML_converter/file-converter.py
+++ 03_markdown_to_HTML_converter/file-converter.py
@@ -8,3 +8,5 @@ def is_markdown():
 def is_valid_inputfile_path():
     return len(sys.argv) > 2 and os.path.exists(sys.argv[2])
 
+def is_inputfile_markdown():
+    return len(sys.argv) > 2 and sys.argv[2].strip().lower().endswith(".md")

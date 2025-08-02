73f0afbddd6f7561f5303594f220c68b87b5f7d1
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-02 14:59:03 +0900

    feat: validate_inputfile function to validate input file and extension

--- 03_markdown_to_HTML_converter/file-converter.py
+++ 03_markdown_to_HTML_converter/file-converter.py
@@ -8,11 +8,26 @@ def is_markdown():
 def is_valid_inputfile_path():
     return len(sys.argv) > 2 and os.path.exists(sys.argv[2])
 
-def is_valid_outputfile_path():
-    return len(sys.argv) > 3 and sys.argv[3].strip() != ""
-
 def is_inputfile_markdown():
     return len(sys.argv) > 2 and sys.argv[2].strip().lower().endswith(".md")
+
+def validate_inputfile():
+    if not is_markdown():
+        print("Please use correct command 'markdown'.")
+        sys.exit(1)
+    
+    if not is_valid_inputfile_path():
+        print("This file does not exist.")
+        sys.exit(1)
+
+    if not is_inputfile_markdown():
+        print("This file is not a markdown file.")
+        sys.exit(1)
+
+    return True
+
+def is_valid_outputfile_path():
+    return len(sys.argv) > 3 and sys.argv[3].strip() != ""
 
 def is_outputfile_html():
     return len(sys.argv) > 3 and sys.argv[3].strip().lower().endswith(".html")
@@ -49,11 +64,11 @@ def convert_markdown_to_html():
     if not is_markdown():
         print("Please use correct command 'markdown'")
         sys.exit(1)
+
+    if not len(sys.argv[3]) > 3:
+        print("Enter output path or file")
     
     md_text = read_markdown_file()
     html = markdown_to_html(md_text)
-    
-    if not len(sys.argv[3]) > 3:
-        print("Enter output path or file")
 
     write_file(sys.argv[3], html)

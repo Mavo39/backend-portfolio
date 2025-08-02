40ecb73ab49e93427610fe506993a318a2694431
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-02 13:53:40 +0900

    feat: add is_valid_outputpath function to validate output path and remove outputfile_already_exists function

--- 03_markdown_to_HTML_converter/file-converter.py
+++ 03_markdown_to_HTML_converter/file-converter.py
@@ -8,11 +8,11 @@ def is_markdown():
 def is_valid_inputfile_path():
     return len(sys.argv) > 2 and os.path.exists(sys.argv[2])
 
+def is_valid_outputfile_path():
+    return len(sys.argv) > 3 and sys.argv[3].strip() != ""
+
 def is_inputfile_markdown():
     return len(sys.argv) > 2 and sys.argv[2].strip().lower().endswith(".md")
-
-def outputfile_already_exists():
-    return len(sys.argv) > 3 and os.path.exists(sys.argv[3])
 
 def read_file(path):
     with open(path, "r", encoding="utf-8") as f:

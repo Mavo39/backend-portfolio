89f1749698e8709c826fe54766b5aec041bd1070
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-09-14 11:39:44 +0900

    refactor: remove '-' between content and size on server.py

--- 04_local_chat_messenger/src/server.py
+++ 04_local_chat_messenger/src/server.py
@@ -24,12 +24,12 @@ while True:
     data_from_client, client_address = udp_server_socket.recvfrom(4096)
     decoded_data = data_from_client.decode('utf-8')
     print("reveived message:")
-    print(f"- client address: {client_address}\n- content: {decoded_data}\n- content-size: {len(data_from_client)} bytes")
+    print(f"- client address: {client_address}\n- content: {decoded_data}\n- content size: {len(data_from_client)} bytes")
 
     # レスポンスデータ作成・送信
     if data_from_client:
         fake_name = fake.name()
         server_sent_message = f"{fake_name} received a message '{decoded_data}'. {fake_name} is from {fake.address()}."
         server_sent_message_bytes = udp_server_socket.sendto(server_sent_message.encode('utf-8'), client_address)
-        print(f"\nserver sent the below\n- content: {server_sent_message}\n- content-size: {server_sent_message_bytes} bytes")
+        print(f"\nserver sent the below\n- content: {server_sent_message}\n- content size: {server_sent_message_bytes} bytes")
     

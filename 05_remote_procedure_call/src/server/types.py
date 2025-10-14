c911cfe4759ce392b89073b6bc3e33078b292cf2
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-10-14 16:13:44 +0900

    feat: Add RPC routing map and type definitions

--- 05_remote_procedure_call/src/server/types.py
+++ 05_remote_procedure_call/src/server/types.py
@@ -1,9 +1,17 @@
 from typing import Dict, Any, List
+from functions import floor, nroot, reverse, validAnagram, sort
 
 type_mapping: Dict[str, Any] = {
     "int": int, 
     "double": float,
-    ""
     "string": str,
     "string[]": List[str]
+}
+
+FUNCTION_MAP = {
+    "floor": floor,
+    "nroot": nroot,
+    "reverse": reverse,
+    "validAnagram": validAnagram,
+    "sort": sort
 }
